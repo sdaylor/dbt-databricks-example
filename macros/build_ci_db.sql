926a4c405f21ccc3a4505fa5c637093aea3cc9d2
Sean Daylor <sdaylor@users.noreply.github.com>
2021-09-28 00:10:56 -0400

    Completed macro for CI db build

--- macros/build_ci_db.sql
+++ macros/build_ci_db.sql
@@ -7,16 +7,33 @@
     {% do log("CI database created: " ~ ci_db_name, info=True) %}
 
     {% set list_table_query %}
-        SHOW TABLES IN {{ prod_db_name }}
+        SHOW TABLE EXTENDED IN {{ prod_db_name }} LIKE '*'
     {% endset %}
-    {% set results_list = run_query(list_table_query).columns["tableName"].values() %}
-    {% do log("Table list fetched" ~ results_list, info=True) %}
+    {% set tables_list = run_query(list_table_query).columns["information"].values() %}
+    {% do log("Tables fetched: " ~ tables_list|length, info=True) %}
 
-    {% for table_name in results_list %}
-        {% set create_table_query %}
-            CREATE OR REPLACE TABLE {{ ci_db_name }}.{{ table_name }} SHALLOW CLONE {{ prod_db_name }}.{{ table_name }};
-        {% endset %}
-        {% do run_query(create_table_query) %}
+    {% for table in tables_list %}
+        {% set ns = namespace() %}
+        {% for attr in table.split('\n') %}
+            {% if attr.startswith('Type:') %}
+                {% set ns.table_type = attr.split(': ')[1] %}
+            {% elif attr.startswith('Table:') %}
+                {% set ns.table_name = attr.split(': ')[1] %}
+            {% elif attr.startswith('View Original Text:') %}
+                {% set ns.view_definition = attr.split(': ')[1] %}
+            {% endif %}
+        {% endfor %}
+        {% if ns.table_type == 'VIEW' %}
+            {% set create_query %}
+                CREATE OR REPLACE VIEW {{ ci_db_name }}.{{ ns.table_name }} AS {{ ns.view_definition }};
+            {% endset %}
+        {% else %}
+            {% set create_query %}
+                CREATE OR REPLACE TABLE {{ ci_db_name }}.{{ ns.table_name }} SHALLOW CLONE {{ prod_db_name }}.{{ ns.table_name }};
+            {% endset %}
+        {% endif %}
+        {% do log("Creating object " ~ ns.table_name ~ " of type " ~ ns.table_type, info=True) %}
+        {% do run_query(create_query) %}
     {% endfor %}
 
 {% endmacro %}
